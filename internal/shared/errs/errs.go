f06a65a0f2dc36460f9b3e78614df52257b7bca8
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2026-02-19 14:06:13 -0300

    ✨ feat(shared/errs): new errs created

--- internal/shared/errs/errs.go
+++ internal/shared/errs/errs.go
@@ -3,11 +3,19 @@ package errs
 import "errors"
 
 var (
+	// General
+	ErrZeroRowsReturned = errors.New("zero rows returned")
 	ErrResourceNotFound = errors.New("resource not found")
 	ErrNothingToUpdate  = errors.New("nothing to update")
 	ErrZeroRowsAffected = errors.New("zero rows affected")
-	ErrResourceConflict = errors.New("resource conflict")
-	ErrUnauthorized     = errors.New("unauthorized")
-	ErrForbidden        = errors.New("forbidden")
 	ErrValidationError  = errors.New("validation error")
+
+	// Auth
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUnauthorized       = errors.New("unauthorized")
+	ErrForbidden          = errors.New("forbidden")
+
+	// Account
+	ErrInactiveAccount = errors.New("account is inactive, cannot perform this action, request its reactivation")
+	ErrDeletedAccount  = errors.New("deleted account, cannot perform this action")
 )
